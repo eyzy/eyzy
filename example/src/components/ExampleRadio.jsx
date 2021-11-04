5e60b3b20c6a3b14fa86d46c97194d068c51516e
amsik <phlyze@gmail.com>
2021-11-04 12:53:53 +0200

    Fix radio

--- example/src/components/ExampleRadio.jsx
+++ example/src/components/ExampleRadio.jsx
@@ -3,16 +3,37 @@ import { Radio } from 'eyzy'
 
 /* eslint-disable */
 
-export default function RadioExamples() {
+export default React.memo(function RadioExamples() {
     const [value, setValue] = useState()
 
     return (
-        <p key="-1">
-          <Radio.Group
-            options={[{ label: 'Абонемент' }, { label: 'Разовый урок' }, { label: 'Групповой урок' }]}
-            onChange={setValue}
-            value={value}
-          />
-        </p>
+        <div>
+            <p>
+              <Radio.Group
+                options={[{ label: 'Абонемент' }, { label: 'Разовый урок' }, { label: 'Групповой урок' }]}
+                onChange={setValue}
+                value={value}
+              />
+            </p>
+            <p className="scr" style={{width: '500px'}}>
+            <Radio.Group
+              name='loong'
+              options={[
+                { label: 'Абонемент', value: '1' }, 
+                { label: 'Разовый урок', value: '2' }, 
+                { label: 'Групповой урок', value: '3' }, 
+                { label: 'Абонемент', value: '4' }, 
+                { label: 'Разовый урок', value: '5' },
+                { label: 'Групповой урок', value: '6' }, 
+                { label: 'Абонемент', value: '7' }, 
+                { label: 'Разовый урок', value: '8' }, 
+                { label: 'Групповой урок', value: '9' }
+              ]}
+              onChange={setValue}
+              value={value}
+            />
+          </p>
+        </div>
     )
 }
+)
